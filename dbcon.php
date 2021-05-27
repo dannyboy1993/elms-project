e338a5d6299c24689455decffb98de29eaf3d1a5
dannyboy1993 <84821349+dannyboy1993@users.noreply.github.com>
2021-05-27 13:22:13 +0800

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -1,3 +1,4 @@
 <?php
-$conn = mysqli_connect('localhost','root','','capstone') or die(mysqli_error());
+//$conn = mysqli_connect('localhost','root','','capstone') or die(mysqli_error());
+$conn = mysqli_connect('remotemysql.com','O5AJu4LvhK','C7ZnVJOgdZ','O5AJu4LvhK');
 ?>
